1f3ce647dc100cc00f0ac10d171ebbce218ed8f4
lanshushui <1256087403@qq.com>
2024-02-19 23:27:29 +0800

    修复依赖中断的问题 50% ---还需要分别implement和api来区分是否是真的传递

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.test
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.library1.Library1Obj
+import com.example.library2.Library2Obj
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +11,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val msg = Library1Obj.getTAG()
         val msg2 = Library1Obj.getLibrary2()
+        val msg3 = Library2Obj.getTAG()
     }
 }
