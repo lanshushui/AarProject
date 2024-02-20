7f8c9e65e041f0f80be2d1de4263c62a2503c6f1
lanshushui <1256087403@qq.com>
2024-02-20 22:10:15 +0800

    修复依赖中断

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.library1.Library1Obj
 import com.example.library2.Library2Obj
+import com.example.library3.Library3Obj
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +12,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val msg = Library1Obj.getTAG()
         val msg2 = Library1Obj.getLibrary2()
-        val msg3 = Library2Obj.getTAG()
+        val msg3 = Library3Obj.getTAG()
     }
 }
