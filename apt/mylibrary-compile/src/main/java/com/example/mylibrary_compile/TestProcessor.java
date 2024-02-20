a4a628171d67d043488efb7443fe1561fd345217
lanshushui <1256087403@qq.com>
2024-02-21 00:16:27 +0800

    新增两个apt模块，为了后续测试

---
+++ apt/mylibrary-compile/src/main/java/com/example/mylibrary_compile/TestProcessor.java
@@ -0,0 +1,26 @@
+package com.example.mylibrary_compile;
+
+import com.example.mylibrary_api.Test;
+import com.google.auto.service.AutoService;
+
+import java.util.Collections;
+import java.util.Set;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Processor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.element.TypeElement;
+
+@AutoService(Processor.class)
+public class TestProcessor extends AbstractProcessor {
+    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+        return Collections.singleton(Test.class.getCanonicalName());
+    }
+
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        return false;
+    }
+}
